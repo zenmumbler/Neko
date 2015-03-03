7a976fefda53675582998c22facaeaa0606a71d0
Arthur Langereis <puurtuur@me.com>
2015-03-03 21:54:46 +0100

    Initial Commit

---
+++ Neko/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Neko
+//
+//  Created by Arthur Langereis on 2015/3/3.
+//  Copyright (c) 2015 logic-dream. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
